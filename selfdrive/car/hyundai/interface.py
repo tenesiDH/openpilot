f2cc5b1544d026cd03d279b69b5654066da335b2
Andrew Frahn <emmertex@gmail.com>
2018-09-21 19:55:50 +1000

    Don't disable when Accelerator Pressed
    The Accelerator no longer cancels OP and Cruise Control
    There may be policy around why comma.ai want this, but in several thousand km of having it, not once was it a desired function, and many many times it caused great annoyance.
    So I killed it, the most annoying Fake Safety feature is dead

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -272,8 +272,7 @@ class CarInterface(object):
       events.append(create_event('pcmDisable', [ET.USER_DISABLE]))
 
     # disable on pedals rising edge or when brake is pressed and speed isn't zero
-    if (ret.gasPressed and not self.gas_pressed_prev) or \
-      (ret.brakePressed and (not self.brake_pressed_prev or ret.vEgoRaw > 0.1)):
+    if (ret.brakePressed and (not self.brake_pressed_prev or ret.vEgoRaw > 0.1)):
       events.append(create_event('pedalPressed', [ET.NO_ENTRY, ET.USER_DISABLE]))
 
     if ret.gasPressed:
