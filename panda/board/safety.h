715fc3a31fba6730b4a961fb57f1e81888409644
Andrew Frahn <emmertex@gmail.com>
2018-10-23 23:20:33 +1100

    Revert "Squashed 'panda/' changes from 5253ab0..4dd3f5a"
    This reverts commit bca47a3dfcb0b71637d945ae5f65bf8f03bdc657.

--- panda/board/safety.h
+++ panda/board/safety.h
@@ -58,7 +58,6 @@ void safety_cb_enable_all();
 #include "safety/safety_toyota.h"
 #ifdef PANDA
 #include "safety/safety_toyota_ipas.h"
-#include "safety/safety_tesla.h"
 #endif
 #include "safety/safety_gm.h"
 #include "safety/safety_ford.h"
@@ -104,7 +103,6 @@ typedef struct {
 #define SAFETY_FORD 5
 #define SAFETY_CADILLAC 6
 #define SAFETY_HYUNDAI 7
-#define SAFETY_TESLA 8
 #define SAFETY_TOYOTA_IPAS 0x1335
 #define SAFETY_TOYOTA_NOLIMITS 0x1336
 #define SAFETY_ALLOUTPUT 0x1337
@@ -123,7 +121,6 @@ const safety_hook_config safety_hook_registry[] = {
   {SAFETY_TOYOTA_NOLIMITS, &toyota_nolimits_hooks},
 #ifdef PANDA
   {SAFETY_TOYOTA_IPAS, &toyota_ipas_hooks},
-  {SAFETY_TESLA, &tesla_hooks},
 #endif
   {SAFETY_ALLOUTPUT, &alloutput_hooks},
   {SAFETY_ELM327, &elm327_hooks},
