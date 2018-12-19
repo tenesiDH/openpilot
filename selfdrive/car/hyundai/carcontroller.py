463d17f64ab2125bb837e0f9088d6b4fd0ea5974
emmertex <andrew@emmertex.com>
2018-12-19 22:51:41 +1100

    Improve reliability of buttom spamming

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -164,7 +164,7 @@ class CarController(object):
     # Ensure the speed limit is within range of the stock cruise control capabilities
     # Do the spamming 10 times a second, we might get from 0 to 10 successful
     # Only do this if we have not yet set the cruise speed
-    if CS.acc_active_real and not self.speed_adjusted and self.map_speed > (8.5 * self.speed_conv) and (self.cnt % 10 == 0):
+    if CS.acc_active_real and not self.speed_adjusted and self.map_speed > (8.5 * self.speed_conv) and (self.cnt % 9 == 0 or self.cnt % 9 == 1):
         # Use some tolerance because of Floats being what they are...
         if (CS.cruise_set_speed * self.speed_conv) > (self.map_speed * 1.005):
             can_sends.append(create_clu11(self.packer, CS.clu11, Buttons.SET_DECEL))
