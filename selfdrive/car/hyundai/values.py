188aad7a2ca760252db97b9ac09191d08bb71db2
Andrew Frahn <emmertex@gmail.com>
2019-01-15 12:20:33 +1100

    Minor Changes
    No need to "use cluster gears" since it is known this does not work
    Alphabetical order for models
    Update to KIA_OPTIMA for consistency

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -18,7 +18,7 @@ class CAR:
   KIA_STINGER = "KIA STINGER GT2 2018"                # First User @killian   <-- Ported by @ku7 (Third)
   SANTA_FE = "HYUNDAI SANTA FE LIMITED 2019"          # First User @rickbias  <-- Ported by @rickbias
   SANTA_FE_2 = "HYUNDAI SANTA FW ULTIMATE 2.0 2019"   # First User @nate      <-- Ported by @ku7
-  OPTIMA = "HYUNDAI OPTIMA SX 2019"                   # First User @blender0
+  KIA_OPTIMA = "KIA OPTIMA SX 2019"                   # First User @blender0
 
 class Buttons:
   NONE = 0
@@ -42,39 +42,42 @@ FINGERPRINTS = {
   CAR.KIA_STINGER: [{
     67: 8, 127: 8, 304: 8, 320: 8, 339: 8, 356: 4, 358: 6, 359: 8, 544: 8, 576: 8, 593: 8, 608: 8, 688: 5, 809: 8, 832: 8, 854: 7, 870: 7, 871: 8, 872: 8, 897: 8, 902: 8, 909: 8, 916: 8, 1040: 8, 1042: 8, 1056: 8, 1057: 8, 1064: 8, 1078: 4, 1107: 5, 1136: 8, 1151: 6, 1168: 7, 1170: 8, 1173: 8, 1184: 8, 1265: 4, 1280: 1, 1281: 4, 1287: 4, 1290: 8, 1292: 8, 1294: 8, 1312: 8, 1322: 8, 1342: 6, 1345: 8, 1348: 8, 1363: 8, 1369: 8, 1378: 4, 1379: 8, 1384: 8, 1407: 8, 1419: 8, 1425: 2, 1427: 6, 1456: 4, 1470: 8
   }],
+  CAR.KIA_OPTIMA: [{
+    64: 8, 66: 8, 67: 8, 68: 8, 127: 8, 273: 8, 274: 8, 275: 8, 339: 8, 356: 4, 399: 8, 447: 8, 512: 6, 544: 8, 593: 8, 608: 8, 688: 5, 790: 8, 809: 8, 832: 8, 884: 8, 897: 8, 899: 8, 902: 8, 903: 6, 909: 8, 916: 8, 1040: 8, 1056: 8, 1057: 8, 1078: 4, 1151: 6, 1168: 7, 1170: 8, 1186: 2, 1191: 2, 1253: 8, 1254: 8, 1255: 8, 1265: 4, 1280: 1, 1282: 4, 1287: 4, 1290: 8, 1292: 8, 1294: 8, 1312: 8, 1322: 8, 1331: 8, 1332: 8, 1333: 8, 1342: 6, 1345: 8, 1348: 8, 1349: 8, 1351: 8, 1353: 8, 1363: 8, 1365: 8, 1366: 8, 1367: 8, 1369: 8, 1407: 8, 1414: 3, 1415: 8, 1419: 8, 1425: 2, 1427: 6, 1440: 8, 1456: 4, 1470: 8, 1472: 8, 1486: 8, 1487: 8, 1491: 8, 1530: 8, 1532: 5, 1952: 8, 1960: 8, 1988: 8, 1996: 8, 2001: 8, 2004: 8, 2008: 8, 2009: 8, 2012: 8, 2016: 8, 2017: 8, 2024: 8, 2025: 8
+  }],
   CAR.SANTA_FE: [{
     67: 8, 127: 8, 304: 8, 320: 8, 339: 8, 356: 4, 544: 8, 593: 8, 608: 8, 688: 6, 809: 8, 832: 8, 854: 7, 870: 7, 871: 8, 872: 8, 897: 8, 902: 8, 903: 8, 905: 8, 909: 8, 916: 8, 1040: 8, 1042: 8, 1056: 8, 1057: 8, 1078: 4, 1107: 5, 1136: 8, 1151: 6, 1155: 8, 1156: 8, 1162: 8, 1164: 8, 1168: 7, 1170: 8, 1173: 8, 1183: 8, 1186: 2, 1191: 2, 1227: 8, 1265: 4, 1280: 1, 1287: 4, 1290: 8, 1292: 8, 1294: 8, 1312: 8, 1322: 8, 1342: 6, 1345: 8, 1348: 8, 1363: 8, 1369: 8, 1379: 8, 1384: 8, 1407: 8, 1414: 3, 1419: 8, 1427: 6, 1456: 4, 1470: 8
   }],
   CAR.SANTA_FE_2: [{
     67: 8, 127: 8, 304: 8, 320: 8, 339: 8, 356: 4, 544: 8, 593: 8, 608: 8, 688: 6, 764: 8, 809: 8, 854: 7, 870: 7, 871: 8, 872: 8, 897: 8, 902: 8, 903: 8, 905: 8, 909: 8, 916: 8, 1040: 8, 1042: 8, 1056: 8, 1057: 8, 1064: 8, 1078: 4, 1107: 5, 1136: 8, 1151: 6, 1155: 8, 1162: 8, 1164: 8, 1168: 7, 1170: 8, 1173: 8, 1180: 8, 1183: 8, 1186: 2, 1227: 8, 1265: 4, 1280: 1, 1287: 4, 1290: 8, 1292: 8, 1294: 8, 1312: 8, 1322: 8, 1345: 8, 1348: 8, 1363: 8, 1369: 8, 1371: 8, 1378: 8, 1384: 8, 1407: 8, 1414: 3, 1419: 8, 1427: 6, 1456: 4, 1470: 8, 1988: 8, 2000: 8, 2004: 8, 2008: 8, 2012: 8
   }],
-  CAR.OPTIMA: [{
-    64: 8, 66: 8, 67: 8, 68: 8, 127: 8, 273: 8, 274: 8, 275: 8, 339: 8, 356: 4, 399: 8, 447: 8, 512: 6, 544: 8, 593: 8, 608: 8, 688: 5, 790: 8, 809: 8, 832: 8, 884: 8, 897: 8, 899: 8, 902: 8, 903: 6, 909: 8, 916: 8, 1040: 8, 1056: 8, 1057: 8, 1078: 4, 1151: 6, 1168: 7, 1170: 8, 1186: 2, 1191: 2, 1253: 8, 1254: 8, 1255: 8, 1265: 4, 1280: 1, 1282: 4, 1287: 4, 1290: 8, 1292: 8, 1294: 8, 1312: 8, 1322: 8, 1331: 8, 1332: 8, 1333: 8, 1342: 6, 1345: 8, 1348: 8, 1349: 8, 1351: 8, 1353: 8, 1363: 8, 1365: 8, 1366: 8, 1367: 8, 1369: 8, 1407: 8, 1414: 3, 1415: 8, 1419: 8, 1425: 2, 1427: 6, 1440: 8, 1456: 4, 1470: 8, 1472: 8, 1486: 8, 1487: 8, 1491: 8, 1530: 8, 1532: 5, 1952: 8, 1960: 8, 1988: 8, 1996: 8, 2001: 8, 2004: 8, 2008: 8, 2009: 8, 2012: 8, 2016: 8, 2017: 8, 2024: 8, 2025: 8
-  }],
 }
 
 CAMERA_MSGS = [832, 1156, 1191, 1342]
 
-# Lane Keep Assist related Features and Limitations
+# Checksum Types
 CHECKSUM = {
-  "6B": [CAR.KIA_SORENTO, CAR.GENESIS],         # 6 Bytes used in Checksum
-  "7B": [CAR.KIA_STINGER, CAR.ELANTRA, CAR.OPTIMA],   # 7 Bytes used in Checksum
-  "crc8": [CAR.SANTA_FE],                       # CRC Checksum
+  "6B": [CAR.KIA_SORENTO, CAR.GENESIS],                 # 6 Bytes used in Checksum
+  "7B": [CAR.KIA_STINGER, CAR.ELANTRA, CAR.OPTIMA],     # 7 Bytes used in Checksum
+  "crc8": [CAR.SANTA_FE],                               # CRC Checksum
 }
+
+# Lane Keep Assist related Features and Limitations
 FEATURES = {
-  "icon_basic": [CAR.GENESIS],                  # Anything but 2 for LKAS_Icon causes MDPS Fault
-  "soft_disable_137": [CAR.GENESIS],            # Any steer message sent below 13.7m/s faults MDPS
-  "soft_disable_168": [CAR.GENESIS_2],          # Any steer message below 16.5m/s faults MDPS
-  "use_cluster_gears": [CAR.ELANTRA, CAR.KIA_SORENTO, CAR.OPTIMA],  # Use Cluster for Gear Selection, rather than Transmission
-  "dnf_mdps": [CAR.SANTA_FE, CAR.SANTA_FE_2],   # Do not forward a modified MDPS message.. different checksum
+  "icon_basic": [CAR.GENESIS],                          # Anything but 2 for LKAS_Icon causes MDPS Fault
+  "soft_disable_137": [CAR.GENESIS],                    # Any steer message sent below 13.7m/s faults MDPS
+  "soft_disable_168": [CAR.GENESIS_2],                  # Any steer message below 16.5m/s faults MDPS
+  "use_cluster_gears": [CAR.ELANTRA, CAR.KIA_SORENTO],  # Use Cluster for Gear Selection, rather than Transmission
+  "dnf_mdps": [CAR.SANTA_FE, CAR.SANTA_FE_2],           # Do not forward a modified MDPS message.. different checksum
 }
+
 DBC = {
   CAR.ELANTRA: dbc_dict('hyundai_kia_generic', None),
   CAR.GENESIS: dbc_dict('hyundai_kia_generic', None),
   CAR.GENESIS_2: dbc_dict('hyundai_kia_generic', None),
   CAR.KIA_SORENTO: dbc_dict('hyundai_kia_generic', None),
   CAR.KIA_STINGER: dbc_dict('hyundai_kia_generic', None),
+  CAR.KIA_OPTIMA: dbc_dict('hyundai_kia_generic', None),
   CAR.SANTA_FE: dbc_dict('hyundai_kia_generic', None),
   CAR.SANTA_FE_2: dbc_dict('hyundai_kia_generic', None),
-  CAR.OPTIMA: dbc_dict('hyundai_kia_generic', None),
 }
