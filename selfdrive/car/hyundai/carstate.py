0cf46eec02ece60ce702a21a09712486a2e34a69
Andrew Frahn <emmertex@gmail.com>
2019-01-24 10:18:20 +1100

    more

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -127,7 +127,8 @@ def get_camera_parser(CP):
     ("CF_Lkas_Unknown2", "LKAS11", 0),
     ("CF_Lkas_ActToi", "LKAS11", 0),
     ("CR_Lkas_StrToqReq", "LKAS11", 0),
-    ("CF_Lkas_MsgCount", "LKAS11", 0)
+    ("CF_Lkas_MsgCount", "LKAS11", 0),
+    ("CF_Lkas_Chksum", "LKAS11", 0)
   ]
 
   checks = [("LKAS11", 100)]
