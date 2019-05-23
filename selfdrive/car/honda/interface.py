3582f6e6db100d2c06c44d952bcb876c3d08591f
kegman <8837066+kegman@users.noreply.github.com>
2019-05-22 21:00:30 -0400

    Fix Insight steerKpV and steerKiV error

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -293,7 +293,7 @@ class CarInterface(object):
       ret.centerToFront = ret.wheelbase * 0.39
       ret.steerRatio = 15  # 12.58 is spec end-to-end
       tire_stiffness_factor = 0.82
-      ret.steerKpV, ret.steerKiV = [[0.6], [0.18]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.18]]
       ret.longitudinalKpBP = [0., 5., 35.]
       ret.longitudinalKpV = [1.2, 0.8, 0.5]
       ret.longitudinalKiBP = [0., 35.]
