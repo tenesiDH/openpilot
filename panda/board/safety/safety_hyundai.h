3625e102fe1192b58c7666dc9d863166738079f9
xx979xx <40252818+xx979xx@users.noreply.github.com>
2019-08-30 04:48:09 +0300

    Update safety_hyundai.h

--- panda/board/safety/safety_hyundai.h
+++ panda/board/safety/safety_hyundai.h
@@ -152,7 +152,9 @@ static int hyundai_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
       // speed address
       if (addr != 593) {
         bus_fwd = 0 + 20;
-      } 
+      } else {
+        bus_fwd = 0;
+      }
     }
   }
   return bus_fwd;
