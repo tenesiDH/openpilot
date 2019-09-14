57bd6ab13a0a3e58cd3604dc684124aa109a1d8e
Arne Schwarck <arneschwarck@gmail.com>
2019-09-14 10:02:35 +0200

    Update start.py

--- selfdrive/mapd/start.py
+++ selfdrive/mapd/start.py
@@ -4,5 +4,5 @@ import os
 assert os.system("make") == 0
 
 if __name__ == "__main__":
-    os.execv("/data/openpilot/mapd.py", ['python3'])
+    os.execv("/data/openpilot/selfdrive/mapd/mapd.py", ['python3'])
 
