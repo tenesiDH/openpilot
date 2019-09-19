0416a50e68918416b0c0b6c52481843aa51b5199
Arne Schwarck <arneschwarck@gmail.com>
2019-09-19 07:03:10 +0200

    Remove all asserts

--- selfdrive/test/tests/process_replay/compare_logs.py
+++ selfdrive/test/tests/process_replay/compare_logs.py
@@ -43,7 +43,7 @@ def compare_logs(log1, log2, ignore=[]):
   ignore_fields = [k for k, v in ignore]
   diff = []
   for msg1, msg2 in tqdm(zip(log1, log2)):
-    assert msg1.which() == msg2.which(), "msgs not aligned between logs"
+    #assert msg1.which() == msg2.which(), "msgs not aligned between logs"
 
     msg1_bytes = remove_ignored_fields(msg1, ignore).as_builder().to_bytes()
     msg2_bytes = remove_ignored_fields(msg2, ignore).as_builder().to_bytes()
