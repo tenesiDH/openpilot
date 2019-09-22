0a7329250afbf963c76131d9849d59a6f1edf448
xx979xx <40252818+xx979xx@users.noreply.github.com>
2019-09-22 12:21:25 +0300

    fix

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -2,7 +2,7 @@ from selfdrive.car import apply_std_steer_torque_limits
 from selfdrive.car.hyundai.hyundaican import create_lkas11, create_lkas12, \
                                              create_1191, create_1156, \
                                              create_clu11, create_mdps12, \
-                                             create_spas11, create_spas12
+                                             create_spas11, create_spas12, create_ems11
 from selfdrive.car.hyundai.values import Buttons
 from selfdrive.can.packer import CANPacker
 import numpy as np
