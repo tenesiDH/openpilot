7d6041ba1b3e97367f13a660b24be44c2eaf0e4d
kegman <8837066+kegman@users.noreply.github.com>
2019-09-24 16:28:12 -0400

    Tuned Honda Pilot kF, steerRatio, and tire_stiffness

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -322,13 +322,14 @@ class CarInterface(object):
       ret.mass = 4204. * CV.LB_TO_KG + STD_CARGO_KG # average weight
       ret.wheelbase = 2.82
       ret.centerToFront = ret.wheelbase * 0.428 # average weight distribution
-      ret.steerRatio = 15.62         # as spec
-      tire_stiffness_factor = 0.444 # not optimized yet
+      ret.steerRatio = 12.85         # Tuned value for 0.6.4 to eliminate wobble
+      tire_stiffness_factor = 0.62 # LiveParameters Value
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.45], [0.21]]
       ret.longitudinalTuning.kpBP = [0., 5., 35.]
       ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5]
       ret.longitudinalTuning.kiBP = [0., 35.]
       ret.longitudinalTuning.kiV = [0.18, 0.12]
+      ret.lateralTuning.pid.kf = 0.00000   #Turn Feed forward off to eliminate wobble
 
     elif candidate == CAR.RIDGELINE:
       stop_and_go = False
