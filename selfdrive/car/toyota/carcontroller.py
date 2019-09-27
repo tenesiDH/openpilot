32266bddec71fc6993d7f2916fd3a15645a720b4
Arne Schwarck <arneschwarck@gmail.com>
2019-09-27 16:33:44 +0200

    rate up equals rate down

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -20,8 +20,8 @@ ACCEL_SCALE = max(ACCEL_MAX, -ACCEL_MIN)
 # Steer torque limits
 class SteerLimitParams:
   STEER_MAX = 1500
-  STEER_DELTA_UP = 10       # 1.5s time to peak torque
-  STEER_DELTA_DOWN = 25     # always lower than 45 otherwise the Rav4 faults (Prius seems ok with 50)
+  STEER_DELTA_UP = 15       # 1.5s time to peak torque
+  STEER_DELTA_DOWN = 15     # always lower than 45 otherwise the Rav4 faults (Prius seems ok with 50)
   STEER_ERROR_MAX = 350     # max delta between torque cmd and torque motor
 
 # Steer angle limits (tested at the Crows Landing track and considered ok)
