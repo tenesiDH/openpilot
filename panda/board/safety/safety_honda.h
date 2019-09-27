62b22a804d9dec840450e9f65320b8927ea9b169
kegman <8837066+kegman@users.noreply.github.com>
2019-09-26 21:44:25 -0400

    Fix - remove bus unused var

--- panda/board/safety/safety_honda.h
+++ panda/board/safety/safety_honda.h
@@ -20,7 +20,6 @@ static void honda_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
 
   int addr = GET_ADDR(to_push);
   int len = GET_LEN(to_push);
-  int bus = GET_BUS(to_push);
 
   // sample speed
   if (addr == 0x158) {
