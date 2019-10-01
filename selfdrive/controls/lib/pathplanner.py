b037f2e4700688e80e04f064ebd72e96610dbe73
Arne Schwarck <arneschwarck@gmail.com>
2019-10-01 15:49:39 +0200

    re test curvature learner

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -9,7 +9,7 @@ from selfdrive.controls.lib.lateral_mpc import libmpc_py
 from selfdrive.controls.lib.drive_helpers import MPC_COST_LAT
 from selfdrive.controls.lib.lane_planner import LanePlanner
 import selfdrive.messaging as messaging
-#from selfdrive.controls.lib.curvature_learner import CurvatureLearner
+from selfdrive.controls.lib.curvature_learner import CurvatureLearner
 
 LOG_MPC = os.environ.get('LOG_MPC', False)
 
@@ -29,8 +29,8 @@ class PathPlanner(object):
     self.setup_mpc(CP.steerRateCost)
     self.solution_invalid_cnt = 0
     self.path_offset_i = 0.0
-    #self.frame = 0
-    #self.curvature_offset = CurvatureLearner(debug=False)
+    self.frame = 0
+    self.curvature_offset = CurvatureLearner(debug=False)
 
   def setup_mpc(self, steer_rate_cost):
     self.libmpc = libmpc_py.libmpc
@@ -60,7 +60,7 @@ class PathPlanner(object):
     # Run MPC
     self.angle_steers_des_prev = self.angle_steers_des_mpc
     VM.update_params(sm['liveParameters'].stiffnessFactor, sm['liveParameters'].steerRatio)
-    curvature_factor = VM.curvature_factor(v_ego)
+    #curvature_factor = VM.curvature_factor(v_ego)
 
     # TODO: Check for active, override, and saturation
     # if active:
@@ -70,11 +70,11 @@ class PathPlanner(object):
     # else:
     #   self.path_offset_i = 0.0
     
-    #if active:
-    #  curvfac = self.curvature_offset.update(angle_steers - angle_offset, self.LP.d_poly, v_ego)
-    #else:
-    #  curvfac = 0.
-    #curvature_factor = VM.curvature_factor(v_ego) + curvfac
+    if active:
+      curvfac = self.curvature_offset.update(angle_steers - angle_offset, self.LP.d_poly, v_ego)
+    else:
+      curvfac = 0.
+    curvature_factor = VM.curvature_factor(v_ego) + curvfac
 
     # account for actuation delay
     self.cur_state = calc_states_after_delay(self.cur_state, v_ego, angle_steers - angle_offset, curvature_factor, VM.sR, CP.steerActuatorDelay)
