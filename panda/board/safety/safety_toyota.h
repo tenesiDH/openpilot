be687bdc5472eca6fcc3580894f70c5543df43e9
Arne Schwarck <arneschwarck@gmail.com>
2019-10-02 08:51:53 +0200

    Try 30 steer ramp down

--- panda/board/safety/safety_toyota.h
+++ panda/board/safety/safety_toyota.h
@@ -4,7 +4,7 @@ const int TOYOTA_MAX_TORQUE = 1500;       // max torque cmd allowed ever
 // rate based torque limit + stay within actually applied
 // packet is sent at 100hz, so this limit is 1000/sec
 const int TOYOTA_MAX_RATE_UP = 10;        // ramp up slow
-const int TOYOTA_MAX_RATE_DOWN = 20;      // ramp down fast
+const int TOYOTA_MAX_RATE_DOWN = 30;      // ramp down fast
 const int TOYOTA_MAX_TORQUE_ERROR = 500;  // max torque cmd in excess of torque motor
 
 // real time torque limit to prevent controls spamming
