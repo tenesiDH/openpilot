261c6b5e0a1e82b1084ac7be0fd4c06c3f35c7ea
kegman <8837066+kegman@users.noreply.github.com>
2019-10-03 23:43:50 -0400

    Fix kegman.json "-1" values for Toyota (#230)
    * Only fetch PID values if PID car
    
    * Fetch sR and sRC for LQR into kegman.json

--- selfdrive/kegman_conf.py
+++ selfdrive/kegman_conf.py
@@ -12,18 +12,23 @@ class kegman_conf():
     if self.conf['tuneGernby'] != "1":
       self.conf['tuneGernby'] = str(1)
       write_conf = True
-    if self.conf['Kp'] == "-1":
-      self.conf['Kp'] = str(round(CP.lateralTuning.pid.kpV[0],3))
-      write_conf = True
-    if self.conf['Ki'] == "-1":
-      self.conf['Ki'] = str(round(CP.lateralTuning.pid.kiV[0],3))
-      write_conf = True
-    if self.conf['Kf'] == "-1":
-      self.conf['Kf'] = str('{:f}'.format(CP.lateralTuning.pid.kf))
-      write_conf = True
+	
+    # only fetch Kp, Ki, Kf sR and sRC from interface.py if it's a PID controlled car
+    if CP.lateralTuning.which() == 'pid':
+      if self.conf['Kp'] == "-1":
+        self.conf['Kp'] = str(round(CP.lateralTuning.pid.kpV[0],3))
+        write_conf = True
+      if self.conf['Ki'] == "-1":
+        self.conf['Ki'] = str(round(CP.lateralTuning.pid.kiV[0],3))
+        write_conf = True
+      if self.conf['Kf'] == "-1":
+        self.conf['Kf'] = str('{:f}'.format(CP.lateralTuning.pid.kf))
+        write_conf = True
+    
     if self.conf['steerRatio'] == "-1":
       self.conf['steerRatio'] = str(round(CP.steerRatio,3))
       write_conf = True
+    
     if self.conf['steerRateCost'] == "-1":
       self.conf['steerRateCost'] = str(round(CP.steerRateCost,3))
       write_conf = True
