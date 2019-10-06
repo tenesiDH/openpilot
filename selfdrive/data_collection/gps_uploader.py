82c3fe33430c955e1f2289039948e3a8643e4e17
Arne Schwarck <arneschwarck@gmail.com>
2019-10-06 21:14:46 +0200

    update with new op params

--- selfdrive/data_collection/gps_uploader.py
+++ selfdrive/data_collection/gps_uploader.py
@@ -4,15 +4,17 @@ import string
 import random
 import os
 from common.params import Params
-import selfdrive.kegman_conf as kegman
+from selfdrive.op_params import opParams
+op_params = opParams()
+uniqueID = op_params.get('uniqueID', None)
 
 def upload_data():
   filepath = "/data/openpilot/selfdrive/data_collection/gps-data"
   if os.path.isfile(filepath):
-    if kegman.get("uniqueID") is None:
-      kegman.save({"uniqueID": ''.join([random.choice(string.lowercase+string.uppercase+string.digits) for i in range(15)])})
+    if uniqueID is None:
+      op_params.put('uniqueID', ''.join([random.choice(string.lowercase+string.uppercase+string.digits) for i in range(15)]))
     try:
-      username = kegman.get("uniqueID", ''.join([random.choice(string.lowercase+string.uppercase+string.digits) for i in range(15)]))
+      username = op_params.get('uniqueID', None)
       try:
         with open("/data/data/ai.comma.plus.offroad/files/persistStore/persist-auth", "r") as f:
           auth = json.loads(f.read())
