904af549b89e00b738419ab011061eb66a8d4f9f
Shane <shane@smiskol.com>
2019-10-05 23:02:22 -0500

    remove from op_params

--- selfdrive/op_params.py
+++ selfdrive/op_params.py
@@ -9,45 +9,6 @@ def write_config(params_file, params):
     json.dump(params, f, indent=2, sort_keys=True)
   os.chmod(params_file, 0o764)
 
-def op_edit():  # use by running `python op_params.py` then `op_edit()`
-  op_params = opParams()
-  params = op_params.params
-  print('Welcome to the opParams command line editor!')
-  print('Here are your parameters:\n')
-  while True:
-    print('\n'.join(['{}. {}: {} ({})'.format(idx + 1, i, params[i], str(type(params[i]))[7:-2]) for idx, i in enumerate(params)]))
-    print('\nChoose a parameter to edit (by index): ')
-    choice = raw_input('>> ')
-    try:
-      choice = int(choice)
-    except:
-      print('Not an integer, exiting!')
-      return
-    if choice not in range(1, len(params)+1):
-      print('Not in range!\n')
-      continue
-    chosen_key = list(params)[choice - 1]
-    old_value = params[chosen_key]
-    print('Chosen parameter: {}'.format(chosen_key))
-    print('Enter your new value:')
-    new_value = raw_input('>> ')
-    try:
-      new_value = ast.literal_eval(new_value)
-      print('New value: {} ({})\nOld value: {} ({})'.format(new_value, str(type(new_value))[7:-2], old_value, str(type(old_value))[7:-2]))
-      print('Do you want to save this?')
-      choice = raw_input('[Y/n]: ').lower()
-      if choice == 'y':
-        op_params.put(chosen_key, new_value)
-        print('Saved! Anything else?')
-        choice = raw_input('[Y/n]: ').lower()
-        if choice == 'n':
-          return
-      else:
-        print('Did not save that...\n')
-    except:
-      print('Cannot parse input, exiting!')
-      break
-
 class opParams:
   def __init__(self):
     self.params_file = "/data/op_params.json"
