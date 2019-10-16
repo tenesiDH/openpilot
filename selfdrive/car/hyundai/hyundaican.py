48763da6140732291da07255903b06b765148592
xx979xx <xx979xx@github.com>
2019-10-16 18:38:23 +0300

    SPAS

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -83,3 +83,55 @@ def create_clu11(packer, clu11, button, cnt):
   }
 
   return packer.make_can_msg("CLU11", 0, values)
+
+
+def create_spas11(packer, car_fingerprint, cnt, en_spas, apply_steer):
+  values = {
+    "CF_Spas_Stat": en_spas,
+    "CF_Spas_TestMode": 0,
+    "CR_Spas_StrAngCmd": apply_steer,
+    "CF_Spas_BeepAlarm": 0,
+    "CF_Spas_Mode_Seq": 2,
+    "CF_Spas_AliveCnt": cnt,
+    "CF_Spas_Chksum": 0,
+    "CF_Spas_PasVol": 0,
+  }
+
+  dat = packer.make_can_msg("SPAS11", 0, values)[2]
+  if car_fingerprint in CHECKSUM["crc8"]:
+    dat = dat[:6]
+    values["CF_Spas_Chksum"] = hyundai_checksum(dat)
+  else:
+    values["CF_Spas_Chksum"] = sum(dat[:6]) % 256
+
+  return packer.make_can_msg("SPAS11", 1, values)
+
+def create_spas12(packer):
+  values = {
+    "CF_Spas_HMI_Stat": 0,
+    "CF_Spas_Disp": 0,
+    "CF_Spas_FIL_Ind": 0,
+    "CF_Spas_FIR_Ind": 0,
+    "CF_Spas_FOL_Ind": 0,
+    "CF_Spas_FOR_Ind": 0,
+    "CF_Spas_VolDown": 0,
+    "CF_Spas_RIL_Ind": 0,
+    "CF_Spas_RIR_Ind": 0,
+    "CF_Spas_FLS_Alarm": 0,
+    "CF_Spas_ROL_Ind": 0,
+    "CF_Spas_ROR_Ind": 0,
+    "CF_Spas_FCS_Alarm": 0,
+    "CF_Spas_FI_Ind": 0,
+    "CF_Spas_RI_Ind": 0,
+    "CF_Spas_FRS_Alarm": 0,
+    "CF_Spas_FR_Alarm": 0,
+    "CF_Spas_RR_Alarm": 0,
+    "CF_Spas_BEEP_Alarm": 0,
+    "CF_Spas_StatAlarm": 0,
+    "CF_Spas_RLS_Alarm": 0,
+    "CF_Spas_RCS_Alarm": 0,
+    "CF_Spas_RRS_Alarm": 0,
+  }
+
+  return packer.make_can_msg("SPAS12", 1, values)
+
