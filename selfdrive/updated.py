f4a891687022c17748c16c623934d227f6e93bb0
Arne Schwarck <arneschwarck@gmail.com>
2019-10-17 13:31:08 +0200

    Use rstrip

--- selfdrive/updated.py
+++ selfdrive/updated.py
@@ -28,28 +28,6 @@ def main(gctx=None):
     if ping_failed:
       time.sleep(60)
       continue
-    if autoUpdate and not os.path.isfile("/data/no_ota_updates"):
-      try:
-        head_commit = subprocess.check_output(["git", "rev-parse", "HEAD"])
-        local_commit = subprocess.check_output(["git", "rev-parse", "@{u}"])
-        if head_commit != local_commit:
-          r = subprocess.check_output(NICE_LOW_PRIORITY + ["git", "pull"], stderr=subprocess.STDOUT)
-          NEED_REBOOT = True
-      except subprocess.CalledProcessError as e:
-        cloudlog.event("git pull failed",
-          cmd=e.cmd,
-          output=e.output,
-          returncode=e.returncode)
-        time.sleep(60)
-        continue
-      cloudlog.info("git pull success: %s", r)
-      if NEED_REBOOT:
-        health = None
-        health = messaging.recv_one_or_none(health_sock)
-        if health is not None:
-          if not health.health.started:
-            NEED_REBOOT = False
-            os.system('reboot')
 
     # download application update
     try:
@@ -70,7 +48,29 @@ def main(gctx=None):
       params.put("UpdateAvailable", str(int(cur_hash != upstream_hash)))
     except:
       params.put("UpdateAvailable", "0")
-
+      
+    if autoUpdate and not os.path.isfile("/data/no_ota_updates"):
+      try:
+        head_commit = subprocess.check_output(["git", "rev-parse", "HEAD"]).rstrip()
+        local_commit = subprocess.check_output(["git", "rev-parse", "@{u}"]).rstrip()
+        if head_commit != local_commit:
+          r = subprocess.check_output(NICE_LOW_PRIORITY + ["git", "pull"], stderr=subprocess.STDOUT)
+          NEED_REBOOT = True
+      except subprocess.CalledProcessError as e:
+        cloudlog.event("git pull failed",
+          cmd=e.cmd,
+          output=e.output,
+          returncode=e.returncode)
+        time.sleep(60)
+        continue
+      cloudlog.info("git pull success: %s", r)
+      if NEED_REBOOT:
+        health = None
+        health = messaging.recv_one_or_none(health_sock)
+        if health is not None:
+          if not health.health.started:
+            NEED_REBOOT = False
+            os.system('reboot')
     # Write latest release notes to param
     try:
       r = subprocess.check_output(["git", "--no-pager", "show", "@{u}:RELEASES.md"])
