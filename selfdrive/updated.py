a37c4014403fb9ba00b86d97d9f266741781bfdf
Arne Schwarck <arneschwarck@gmail.com>
2019-10-17 14:13:00 +0200

    reformat autoupdate to use comma params

--- selfdrive/updated.py
+++ selfdrive/updated.py
@@ -46,16 +46,10 @@ def main(gctx=None):
       cur_hash = subprocess.check_output(["git", "rev-parse", "HEAD"]).rstrip()
       upstream_hash = subprocess.check_output(["git", "rev-parse", "@{u}"]).rstrip()
       params.put("UpdateAvailable", str(int(cur_hash != upstream_hash)))
-    except:
-      params.put("UpdateAvailable", "0")
-      
-    if autoUpdate and not os.path.isfile("/data/no_ota_updates"):
       try:
-        head_commit = subprocess.check_output(["git", "rev-parse", "HEAD"]).rstrip()
-        local_commit = subprocess.check_output(["git", "rev-parse", "@{u}"]).rstrip()
-        if head_commit != local_commit:
+        if autoUpdate and not os.path.isfile("/data/no_ota_updates") and cur_hash != upstream_hash:
           r = subprocess.check_output(NICE_LOW_PRIORITY + ["git", "pull"], stderr=subprocess.STDOUT)
-          NEED_REBOOT = True
+          NEED_REBOOT = True        
       except subprocess.CalledProcessError as e:
         cloudlog.event("git pull failed",
           cmd=e.cmd,
@@ -64,13 +58,9 @@ def main(gctx=None):
         time.sleep(60)
         continue
       cloudlog.info("git pull success: %s", r)
-      if NEED_REBOOT:
-        health = None
-        health = messaging.recv_one_or_none(health_sock)
-        if health is not None:
-          if not health.health.started:
-            NEED_REBOOT = False
-            os.system('reboot')
+    except:
+      params.put("UpdateAvailable", "0")
+
     # Write latest release notes to param
     try:
       r = subprocess.check_output(["git", "--no-pager", "show", "@{u}:RELEASES.md"])
@@ -81,6 +71,14 @@ def main(gctx=None):
 
     t = datetime.datetime.now().isoformat()
     params.put("LastUpdateTime", t.encode('utf8'))
+    
+    if NEED_REBOOT:
+      health = None
+      health = messaging.recv_one_or_none(health_sock)
+      if health is not None:
+        if not health.health.started:
+          NEED_REBOOT = False
+          os.system('reboot')
 
     time.sleep(30*60)
 
