3f8d31782139643f12f84739449ab029ab7bec05
Arne Schwarck <arneschwarck@gmail.com>
2019-10-17 12:01:33 +0200

    if ignition is off and new code available
    Add Back auto update and reboot.
    Check to see if the ignition is on before rebooting

--- selfdrive/updated.py
+++ selfdrive/updated.py
@@ -5,14 +5,23 @@
 import datetime
 import subprocess
 import time
+import os
 
 from common.params import Params
 from selfdrive.swaglog import cloudlog
+from selfdrive.op_params import opParams
+import selfdrive.messaging as messaging
+from selfdrive.services import service_list
+
+op_params = opParams()
+
+autoUpdate = op_params.get('autoUpdate', True)
 
 NICE_LOW_PRIORITY = ["nice", "-n", "19"]
 def main(gctx=None):
   params = Params()
-
+  NEED_REBOOT = False
+  health_sock = messaging.sub_sock(service_list['health'].port, conflate=True)
   while True:
     # try network
     ping_failed = subprocess.call(["ping", "-W", "4", "-c", "1", "8.8.8.8"])
@@ -50,8 +59,30 @@ def main(gctx=None):
 
     t = datetime.datetime.now().isoformat()
     params.put("LastUpdateTime", t.encode('utf8'))
+    if autoUpdate and not os.path.isfile("/data/no_ota_updates"):
+      try:
+        head_commit = subprocess.check_output(["git", "rev-parse", "HEAD"])
+        local_commit = subprocess.check_output(["git", "rev-parse", "@{u}"])
+        if head_commit != local_commit:
+          r = subprocess.check_output(NICE_LOW_PRIORITY + ["git", "pull"], stderr=subprocess.STDOUT)
+          NEED_REBOOT = True
+      except subprocess.CalledProcessError as e:
+        cloudlog.event("git pull failed",
+          cmd=e.cmd,
+          output=e.output,
+          returncode=e.returncode)
+        time.sleep(60)
+        continue
+      cloudlog.info("git pull success: %s", r)
+      if NEED_REBOOT:
+        health = None
+        health = messaging.recv_one_or_none(health_sock)
+        if health is not None:
+          if not health.health.started:
+            NEED_REBOOT = False
+            os.system('reboot')
 
-    time.sleep(60*60)
+    time.sleep(30*60)
 
 if __name__ == "__main__":
   main()
