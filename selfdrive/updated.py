4c04aa35442742ebba1c7fe186ee3c08988707bd
Arne Schwarck <arneschwarck@gmail.com>
2019-10-17 13:25:50 +0200

    reverse order for auto update

--- selfdrive/updated.py
+++ selfdrive/updated.py
@@ -28,6 +28,28 @@ def main(gctx=None):
     if ping_failed:
       time.sleep(60)
       continue
+    if autoUpdate and not os.path.isfile("/data/no_ota_updates"):
+      try:
+        head_commit = subprocess.check_output(["git", "rev-parse", "HEAD"])
+        local_commit = subprocess.check_output(["git", "rev-parse", "@{u}"])
+        if head_commit != local_commit:
+          r = subprocess.check_output(NICE_LOW_PRIORITY + ["git", "pull"], stderr=subprocess.STDOUT)
+          NEED_REBOOT = True
+      except subprocess.CalledProcessError as e:
+        cloudlog.event("git pull failed",
+          cmd=e.cmd,
+          output=e.output,
+          returncode=e.returncode)
+        time.sleep(60)
+        continue
+      cloudlog.info("git pull success: %s", r)
+      if NEED_REBOOT:
+        health = None
+        health = messaging.recv_one_or_none(health_sock)
+        if health is not None:
+          if not health.health.started:
+            NEED_REBOOT = False
+            os.system('reboot')
 
     # download application update
     try:
@@ -59,28 +81,6 @@ def main(gctx=None):
 
     t = datetime.datetime.now().isoformat()
     params.put("LastUpdateTime", t.encode('utf8'))
-    if autoUpdate and not os.path.isfile("/data/no_ota_updates"):
-      try:
-        head_commit = subprocess.check_output(["git", "rev-parse", "HEAD"])
-        local_commit = subprocess.check_output(["git", "rev-parse", "@{u}"])
-        if head_commit != local_commit:
-          r = subprocess.check_output(NICE_LOW_PRIORITY + ["git", "pull"], stderr=subprocess.STDOUT)
-          NEED_REBOOT = True
-      except subprocess.CalledProcessError as e:
-        cloudlog.event("git pull failed",
-          cmd=e.cmd,
-          output=e.output,
-          returncode=e.returncode)
-        time.sleep(60)
-        continue
-      cloudlog.info("git pull success: %s", r)
-      if NEED_REBOOT:
-        health = None
-        health = messaging.recv_one_or_none(health_sock)
-        if health is not None:
-          if not health.health.started:
-            NEED_REBOOT = False
-            os.system('reboot')
 
     time.sleep(30*60)
 
