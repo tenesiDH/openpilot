cc04b6d0cc63be710399e09bffdf9f4bfe797cb3
Shane <shane@smiskol.com>
2019-10-22 15:47:46 -0500

    send uniqueid with sentry crash report

--- common/op_params.py
+++ common/op_params.py
@@ -1,6 +1,8 @@
 import os
 import json
 import time
+import string
+import random
 from common.travis_checker import travis
 
 
@@ -29,13 +31,25 @@ class opParams:
     self.last_read_time = time.time()
     self.read_timeout = 1.0  # max frequency to read with self.get(...) (sec)
     self.default_params = {'camera_offset': 0.06, 'awareness_factor': 2.0, 'lane_hug_direction': None, 'lane_hug_mod': 1.2, 'lane_hug_angle': 10}
+    self.force_update = False  # replaces values with default params if True, not just add add missing key/value pairs
     self.run_init()  # restores, reads, and updates params
 
-  def add_default_params(self, force_update=False):
+  def create_id(self):  # creates unique identifier to send with sentry errors. please update uniqueID with op_edit.py to your username!
+    need_id = False
+    if "uniqueID" not in self.params:
+      need_id = True
+    if "uniqueID" in self.params and self.params["uniqueID"] is None:
+      need_id = True
+    if need_id:
+      random_id = ''.join([random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for i in range(15)])
+      self.params["uniqueID"] = random_id
+
+  def add_default_params(self):
     prev_params = dict(self.params)
     if not travis:
+      self.create_id()
       for i in self.default_params:
-        if force_update:
+        if self.force_update:
           self.params.update({i: self.default_params[i]})
         elif i not in self.params:
           self.params.update({i: self.default_params[i]})
@@ -47,14 +61,13 @@ class opParams:
       self.params = self.default_params
       self.add_default_params()
       return
-    force_update = False  # replaces values with default params if True, not just add add missing key/value pairs
     self.params = self.default_params  # in case any file is corrupted
     to_write = False
     no_params = False
     if os.path.isfile(self.params_file):
       self.params, read_status = read_params(self.params_file, self.default_params)
       if read_status:
-        to_write = not self.add_default_params(force_update=force_update)  # if new default data has been added
+        to_write = not self.add_default_params()  # if new default data has been added
       else:  # don't overwrite corrupted params, just print to screen
         print("ERROR: Can't read op_params.json file")
     elif os.path.isfile(self.kegman_file):
@@ -62,7 +75,7 @@ class opParams:
       try:
         with open(self.kegman_file, "r") as f:  # restore params from kegman
           self.params = json.load(f)
-          self.add_default_params(force_update=force_update)
+          self.add_default_params()
       except:
         print("ERROR: Can't read kegman.json file")
     else:
