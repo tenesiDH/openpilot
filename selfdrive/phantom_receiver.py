d11c46afffcdaf8599198d726f45021fd4008f44
Shane <shane@smiskol.com>
2019-11-06 19:16:15 -0600

    updates to phantom_receiver

--- selfdrive/phantom_receiver.py
+++ selfdrive/phantom_receiver.py
@@ -4,27 +4,31 @@ import selfdrive.messaging_arne as messaging_arne
 
 class PhantomReceiver:
   def __init__(self):
-    self.phantomData_sock = None
+    self.phantom_data_sock = None
 
-  def broadcast_data(self, speed, angle, time):
+  def receive_data(self, speed, angle, time):
+    self.broadcast_data(True, speed, angle, time)
+
+  def broadcast_data(self, status, speed, angle, time):
     with open('/data/bd.test', 'a') as f:
-      f.write('{}\n'.format([speed, angle, time]))
-    data = messaging_arne.new_message()
-    data.init('phantomData')
-    data.phantomData.status = True
-    data.phantomData.speed = speed
-    data.phantomData.angle = angle
-    data.phantomData.time = time
-    self.phantomData_sock.send(data.to_bytes())
+      f.write('{}\n'.format([status, speed, angle, time]))
+    phantom_data = messaging_arne.new_message()
+    phantom_data.init('phantomData')
+    phantom_data.phantomData.status = status
+    phantom_data.phantomData.speed = speed
+    phantom_data.phantomData.angle = angle
+    phantom_data.phantomData.time = time
+    self.phantom_data_sock.send(phantom_data.to_bytes())
 
   def enable_phantom(self):
     with open('/data/bd.test', 'a') as f:
       f.write('{}\n'.format('open socket'))
-    #self.phantomData_sock = messaging_arne.pub_sock(service_list['phantomData'].port)
-    return "ENABLED"
+    self.phantom_data_sock = messaging_arne.pub_sock(service_list['phantomData'].port)
+    return "ENABLED"  # needed to tell the app we're all done with this function
 
   def disable_phantom(self):
     with open('/data/bd.test', 'a') as f:
       f.write('{}\n'.format('close socket'))
-    #self.phantomData_sock.close()
+    self.broadcast_data(False, 0.0, 0.0, 0.0)
+    self.phantom_data_sock.close()
     return "DISABLED"
