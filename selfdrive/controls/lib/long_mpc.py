d5d9f9a02a77b9f09cce3cd05a06ad209f0532f7
Arne Schwarck <arneschwarck@gmail.com>
2019-11-09 08:39:56 +0100

    Relative velocity and not additive

--- selfdrive/controls/lib/long_mpc.py
+++ selfdrive/controls/lib/long_mpc.py
@@ -121,7 +121,7 @@ class LongitudinalMpc():
     if self.v_lead is not None:  # since the new mpc now handles braking nicely, simplify mods
       x = [0, 0.61, 1.26, 2.1, 2.68]  # relative velocity values
       y = [0, -0.017, -0.053, -0.154, -0.272]  # modification values
-      TR_mod = interp(self.v_lead + self.v_ego, x, y)  # quicker acceleration/don't brake when lead is overtaking
+      TR_mod = interp(self.v_lead - self.v_ego, x, y)  # quicker acceleration/don't brake when lead is overtaking
 
       '''x = [-1.49, -1.1, -0.67, 0.0, 0.67, 1.1, 1.49]
       y = [0.056, 0.032, 0.016, 0.0, -0.016, -0.032, -0.056]
