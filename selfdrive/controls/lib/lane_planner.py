5f3bb7b3081b2f7884df4cb40c0a450074207665
Arne Schwarck <arneschwarck@gmail.com>
2019-11-11 08:16:40 +0100

    Lane width is correctly identified

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -76,7 +76,7 @@ class LanePlanner():
     speed_lane_width = interp(v_ego, [0., 14., 20.], [2.5, 3., 3.5]) # German Standards
     self.lane_width = self.lane_width_certainty * self.lane_width_estimate + \
                       (1 - self.lane_width_certainty) * speed_lane_width
-    print(current_lane_width)
+    #print(current_lane_width)
     self.d_poly = calc_d_poly(self.l_poly, self.r_poly, self.p_poly, self.l_prob, self.r_prob, self.lane_width)
 
   def update(self, v_ego, md):
