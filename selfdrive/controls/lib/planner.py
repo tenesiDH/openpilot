39b056d8323baaff1cbfa67594ef1e47797f00d9
Arne Schwarck <arneschwarck@gmail.com>
2019-11-26 12:41:23 +0100

    Add a missing 2

--- selfdrive/controls/lib/planner.py
+++ selfdrive/controls/lib/planner.py
@@ -120,7 +120,7 @@ class Planner():
   def choose_solution(self, v_cruise_setpoint, enabled, yRel, dRel, steeringAngle):
     if enabled:
       solutions = {'model': self.v_model, 'cruise': self.v_cruise}
-      if self.mpc1.prev_lead_status and (abs(math.atan(yRel,dRel)*180/math.pi - steeringAngle/10) < max(math.atan2(1,dRel)*180/math.pi, 5.0)):
+      if self.mpc1.prev_lead_status and (abs(math.atan2(yRel,dRel)*180/math.pi - steeringAngle/10) < max(math.atan2(1,dRel)*180/math.pi, 5.0)):
         solutions['mpc1'] = self.mpc1.v_mpc
       if self.mpc2.prev_lead_status:
         solutions['mpc2'] = self.mpc2.v_mpc
