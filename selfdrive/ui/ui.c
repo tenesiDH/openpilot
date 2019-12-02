d30a6ddf47a157da5de7ede6b7527ca7c5b4fc74
xx979xx <40252818+xx979xx@users.noreply.github.com>
2019-12-02 20:34:15 +0300

    show no SteersDes if OP disangaged

--- selfdrive/ui/ui.c
+++ selfdrive/ui/ui.c
@@ -1376,6 +1376,7 @@ static void bb_ui_draw_measures_right(UIState *s, int bb_x, int bb_y, int bb_w )
     char val_str[16];
     char uom_str[6];
     NVGcolor val_color = nvgRGBA(255, 255, 255, 200);
+    if (scene->engaged) {
       //show Orange if more than 6 degrees
       //show red if  more than 12 degrees
       if(((int)(scene->angleSteersDes) < -6) || ((int)(scene->angleSteersDes) > 6)) {
@@ -1386,7 +1387,9 @@ static void bb_ui_draw_measures_right(UIState *s, int bb_x, int bb_y, int bb_w )
       }
       // steering is in degrees
       snprintf(val_str, sizeof(val_str), "%.0f°",(scene->angleSteersDes));
-
+    } else {
+       snprintf(val_str, sizeof(val_str), "-");
+    }
       snprintf(uom_str, sizeof(uom_str), "");
     bb_h +=bb_ui_draw_measure(s,  val_str, uom_str, "DESIR STEER",
         bb_rx, bb_ry, bb_uom_dx,
