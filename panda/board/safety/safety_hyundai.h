adfffc521c466efbd2a4604559b6c047e0208857
xx979xx <40252818+xx979xx@users.noreply.github.com>
2019-12-12 22:33:14 +0300

    stop SCC12 forwarding

--- panda/board/safety/safety_hyundai.h
+++ panda/board/safety/safety_hyundai.h
@@ -169,7 +169,11 @@ static int hyundai_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
     if (bus_num == hyundai_camera_bus) {
       int addr = GET_ADDR(to_fwd);
       if (addr != 832) {
-        bus_fwd = 10;
+        if ((!OP_LKAS_live) || (addr != 1057)) {
+          bus_fwd = 10;
+        } else {
+          bus_fwd = 1;
+        }
       }
       else if (!OP_LKAS_live) {
         hyundai_LKAS_forwarded = 1;
