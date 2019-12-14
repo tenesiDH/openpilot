5391c1cfba46978ec351b56ce5132d23eb424bbc
xx979xx <40252818+xx979xx@users.noreply.github.com>
2019-12-15 01:00:39 +0300

    MDPS forwarding

--- panda/board/safety/safety_defaults.h
+++ panda/board/safety/safety_defaults.h
@@ -22,9 +22,18 @@ static int nooutput_tx_lin_hook(int lin_num, uint8_t *data, int len) {
 }
 
 static int default_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
-  UNUSED(bus_num);
   UNUSED(to_fwd);
-  return -1;
+  int bus_fwd = -1;
+  if (bus_num == 0) {
+    bus_fwd = 1;
+  }
+  if (bus_num == 1) {
+    bus_fwd = 20;
+  }
+  if (bus_num == 2) {
+    bus_fwd = 1;
+  }
+  return bus_fwd;
 }
 
 const safety_hooks nooutput_hooks = {
